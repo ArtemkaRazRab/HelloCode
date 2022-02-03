1a6d278dc7d8dccd522396b23a77faed28d08f96
«Artemka» <sergeev_artem91@mail.ru>
2022-02-04 02:56:44 +0500

    Example 014 ArrayMethod_3

---
+++ Example014_ArrayMethod_3/Program.cs
@@ -0,0 +1,46 @@
+﻿int[] array = new int[10];
+
+void FillArray(int[] collection)
+{
+    int length = collection.Length;
+    int index = 0;
+    while (index < length)
+    {
+        collection[index] = new Random().Next(1, 10);
+        index = index + 1;
+    }
+}
+void PrintArray(int[] col)
+{
+    int count = col.Length;
+    int position = 0;
+    while (position < count)
+    {
+        Console.WriteLine(col[position]);
+        position = position + 1;
+    }
+}
+int Indexof(int[] collection, int find)
+{
+    int count = collection.Length;
+    int index = 0;
+    int position = -1;
+    while (index < count)
+    {
+        if (collection[index] == find)
+        {
+            position = index;
+            break;
+        }
+        index = index + 1;
+    }
+    return position;
+}
+FillArray(array);
+array[4] = 4;  // это для проверки
+array[6] = 4;  // это для проверки
+PrintArray(array);
+Console.WriteLine();
+
+int pos = Indexof(array, 4);
+Console.WriteLine(pos);
