a6d0986115463958806a85dfc44462afb109da41
«Artemka» <sergeev_artem91@mail.ru>
2022-02-07 07:33:39 +0500

    Method 2

---
+++ Example016_Methods_2/Program.cs
@@ -0,0 +1,24 @@
+﻿// Вид 2 (что-то принимают, ничего не возвращают)
+
+// Первый способ
+
+void Method2(string msg)
+{
+    Console.WriteLine(msg);
+}
+//Method2("Текст сообщения");
+
+// Второй способ
+
+void Method21(string msg, int count) // msg - сообщение, count - считать
+{
+    int i = 0;
+    while (i < count)
+    {
+        Console.WriteLine(msg);
+        i++;
+    }
+}
+Method21(msg: "Текст сообщения", count: 4);
+// Method21("Текст сообщения", 4);  можно писать так
+// Method21(count: 4, msg: "Текст сообщения");  можно писать так
